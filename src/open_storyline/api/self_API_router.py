5c221de1e60f4d7f2e012e20535dd430c311d868
Sodier-zhang <1329118407@qq.com>
2026-08-24 14:24:28 +0800

    修改旧路径相关内容，无警告显示

---
+++ src/open_storyline/api/self_API_router.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+from open_storyline.api.Yuanji_API_router import *  # noqa: F403
+
---
+++ src/open_storyline/api/self_API_service.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+from open_storyline.api.Yuanji_API_service import *  # noqa: F403
+
